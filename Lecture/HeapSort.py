1c5becb7f9e7e59ad3f07526a1d3c14c01c92c94
shawnwoo27 <57933006+shawnwoo27@users.noreply.github.com>
2021-10-07 10:40:50 +0900

    update sort

--- Lecture/HeapSort.py
+++ Lecture/HeapSort.py
@@ -2,20 +2,26 @@ a = [7, 9, 4, 8, 6, 3]
 
 
 def build_heap(a, n):
-    for i in range(n//2, 0, -1):
+    # 마지막 인덱스의 부모 노드 부터 heapify 시작
+    for i in range(n//2, -1, -1):
         heapify(a, i, n)
 
 
 def heap_sort(a, n):
     build_heap(a, n)
-    for i in range(n, 0, -1):
+
+    # 최상위 노드 값이 가장 작으므로
+    # 최상위 노드를 출력 후, 맨끝 값하고 바꾸고
+    # heapify 를 통해 최상위 노드가 항상 가장 작은 상태를 유지
+    for i in range(n, -1, -1):
+        print(a[0])
         a[0], a[i] = a[i], a[0]
         heapify(a, 0, i-1)
 
 
 def heapify(a, k, n):
-    left = 2*k + 1
-    right = 2*k + 2
+    left = 2*k + 1  # 왼쪽 자식 노드
+    right = 2*k + 2  # 오른쪽 자식 노드
 
     if right <= n:
         if a[left] < a[right]:
@@ -35,4 +41,4 @@ def heapify(a, k, n):
 
 
 heap_sort(a, len(a)-1)
-print(a)
+
--- Lecture/QuickSort.py
+++ Lecture/QuickSort.py
@@ -2,6 +2,7 @@ a = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 
 def quick_sort(a, p, r):
+    # 파티션을 기준으로 왼쪽 오른쪽으로 반복 수행
     if p < r:
         q = partition(a, p, r)
         quick_sort(a, p, q-1)
@@ -10,20 +11,25 @@ def quick_sort(a, p, r):
 
 def partition(a, p, r):
     pivot = p
-    left = p + 1
+    left = p + 1  # p는 pivot이라 +1부터 확인
     right = r
 
+    # L, R이 엇갈리지 않을 때까지 반복
     while right >= left:
+        # 엇갈리지 않는 선에서, pivot 보다 큰 값의 index 찾기 
         while left <= r and a[left] <= a[pivot]:
             left += 1
-
+            
+        # 엇갈리지 않는 선에서, pivot 보다 큰 값의 index 찾기
         while right > p and a[right] >= a[pivot]:
             right -= 1
 
+        # 엇갈리면 pivot과 작은 값을 바꾸고, 작은 값이 있던 index 리턴
         if left > right:
             a[pivot], a[right] = a[right], a[pivot]
             return right
 
+        # 엇갈리지 않았으면, 찾은 index의 원소를 뒤바꿔줌
         else:
             a[left], a[right] = a[right], a[left]
 
