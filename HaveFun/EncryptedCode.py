fe33d3ed96211a326d1d10d487abc3e32758ba58
shawnwoo27 <57933006+shawnwoo27@users.noreply.github.com>
2021-11-18 23:16:02 +0900

    freind question

---
+++ HaveFun/EncryptedCode.py
@@ -0,0 +1,38 @@
+in_filename = input("암호화할 파일명:")
+out_filename = input("암호화된 문서의 파일명:")
+
+in_file = open(in_filename, "rt", encoding="utf-8")
+in_file = open(out_filename, "wt", encoding="utf-8")
+
+while True:
+    inStr = in_file.readline()
+    if (inStr == ""):
+        break
+
+    outStr = ""
+    for ch in inStr :
+        encoding_ch = chr(ord(ch)+100)
+        outStr = outStr + encoding_ch
+    out_file.write(outStr)
+
+in_file.close()
+out.flie.close()
+# in_filename = input("암호화할 파일명:")
+# out_filename = input("암호화된 문서의 파일명:")
+#
+# in_file = open(in_filename, "rt", encoding="utf-8")
+# out_file = open(out_filename, "wt", encoding="utf-8")
+#
+# while True:
+#     inStr = in_file.readline()
+#
+#     if inStr == "":
+#         break
+#
+#     outStr = ""
+#     for ch in inStr:
+#         encoding_ch = chr(ord(ch)+100)
+#         outStr = outStr + encoding_ch
+#     out_file.write(outStr)
+#
+# in_file.close()
